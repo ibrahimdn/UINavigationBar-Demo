00d05a29fe40e80278fe20e4cd7e5fe736609920
ibrahim doğan <ibrahim.dogan@digitus.com.tr>
2019-06-27 15:05:46 +0300

    Initial Commit

---
+++ navigation.Demo/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  navigation.Demo
+//
+//  Created by ibrahim doğan on 27.06.2019.
+//  Copyright © 2019 Tabbar. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ navigation.Demo/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  navigation.Demo
+//
+//  Created by ibrahim doğan on 27.06.2019.
+//  Copyright © 2019 Tabbar. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
