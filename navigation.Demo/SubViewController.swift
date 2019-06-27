99be2f897355e7b0b2836f0ceff06287adbf73d7
ibrahim doğan <ibrahim.dogan@digitus.com.tr>
2019-06-27 15:47:54 +0300

    first commit

---
+++ navigation.Demo/SubViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SubViewController.swift
+//  navigation.Demo
+//
+//  Created by ibrahim doğan on 27.06.2019.
+//  Copyright © 2019 Tabbar. All rights reserved.
+//
+
+import UIKit
+
+class SubViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
